f2ed6e8aa1470ef6c082f801dbc8c41d638c3f14
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2018-04-07 17:04:55 +0900

    first answer

--- lib/ord23e.ex
+++ lib/ord23e.ex
@@ -1,18 +1,76 @@
 defmodule Ord23e do
-  @moduledoc """
-  Documentation for Ord23e.
-  """
+  def solve(input) do
+    {index, rules} =
+      input
+      |> String.split(",")
+      |> conv()
 
-  @doc """
-  Hello world.
+    total_length = total_length(rules)
 
-  ## Examples
+    if total_length < index do
+      "x"
+    else
+      {_, _, d} =
+        rules
+        |> Enum.reduce({index - 1, total_length, 0}, fn rule, {index, length, d} ->
+          case rule do
+            ?a ->
+              length = div(length, 4)
 
-      iex> Ord23e.hello
-      :world
+              d =
+                case div(index, length) do
+                  0 -> d
+                  1 -> d + 1
+                  2 -> d - 1
+                  3 -> d
+                end
 
-  """
-  def hello do
-    :world
+              {rem(index, length), length, d}
+
+            ?b ->
+              length = div(length, 5)
+
+              d =
+                case div(index, length) do
+                  0 -> d
+                  1 -> d + 1
+                  2 -> d
+                  3 -> d - 1
+                  4 -> d
+                end
+
+              {rem(index, length), length, d}
+          end
+        end)
+
+      case rem(d, 6) do
+        -4 -> "-"
+        -3 -> "0"
+        -2 -> "+"
+        -1 -> "-"
+        0 -> "0"
+        1 -> "+"
+        2 -> "-"
+        3 -> "0"
+        4 -> "+"
+      end
+    end
+  end
+
+  def conv([index, rules]) do
+    {
+      String.to_integer(index),
+      String.to_charlist(rules)
+    }
+  end
+
+  def total_length(rules) do
+    rules
+    |> Enum.reduce(1, fn rule, acc ->
+      case rule do
+        ?a -> acc * 4
+        ?b -> acc * 5
+      end
+    end)
   end
 end
